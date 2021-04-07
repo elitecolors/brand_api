5c50d1060fec15b1f5be2b50f72bcd67eaf1b850
elitecolors <saidaniahmed125@gmail.com>
2021-04-07 13:58:11 +0200

    create entity contact

---
+++ src/Entity/Contact.php
@@ -0,0 +1,330 @@
+<?php
+
+namespace App\Entity;
+
+use App\Repository\ContactRepository;
+use Doctrine\ORM\Mapping as ORM;
+
+/**
+ * @ORM\Entity(repositoryClass=ContactRepository::class)
+ */
+class Contact
+{
+    /**
+     * @ORM\Id
+     * @ORM\GeneratedValue
+     * @ORM\Column(type="integer")
+     */
+    private $id;
+
+    /**
+     * @ORM\Column(type="string", length=255)
+     */
+    private $user_name;
+
+    /**
+     * @ORM\Column(type="string", length=255)
+     */
+    private $name_prefix;
+
+    /**
+     * @ORM\Column(type="string", length=255)
+     */
+    private $first_name;
+
+    /**
+     * @ORM\Column(type="string", length=255)
+     */
+    private $midle_name;
+
+    /**
+     * @ORM\Column(type="string", length=255)
+     */
+    private $last_name;
+
+    /**
+     * @ORM\Column(type="string", length=255)
+     */
+    private $gender;
+
+    /**
+     * @ORM\Column(type="string", length=255)
+     */
+    private $email;
+
+    /**
+     * @ORM\Column(type="date")
+     */
+    private $date_birth;
+
+    /**
+     * @ORM\Column(type="time")
+     */
+    private $time_birth;
+
+    /**
+     * @ORM\Column(type="dateinterval")
+     */
+    private $age_birth;
+
+    /**
+     * @ORM\Column(type="date", nullable=true)
+     */
+    private $date_join;
+
+    /**
+     * @ORM\Column(type="dateinterval", nullable=true)
+     */
+    private $age_in_company;
+
+    /**
+     * @ORM\Column(type="string", length=255, nullable=true)
+     */
+    private $phone;
+
+    /**
+     * @ORM\Column(type="string", length=255, nullable=true)
+     */
+    private $place;
+
+    /**
+     * @ORM\Column(type="string", length=255, nullable=true)
+     */
+    private $country;
+
+    /**
+     * @ORM\Column(type="string", length=255, nullable=true)
+     */
+    private $city;
+
+    /**
+     * @ORM\Column(type="string", length=255, nullable=true)
+     */
+    private $zip;
+
+    /**
+     * @ORM\Column(type="string", length=255, nullable=true)
+     */
+    private $region;
+
+    public function getId(): ?int
+    {
+        return $this->id;
+    }
+
+    public function getUserName(): ?string
+    {
+        return $this->user_name;
+    }
+
+    public function setUserName(string $user_name): self
+    {
+        $this->user_name = $user_name;
+
+        return $this;
+    }
+
+    public function getNamePrefix(): ?string
+    {
+        return $this->name_prefix;
+    }
+
+    public function setNamePrefix(string $name_prefix): self
+    {
+        $this->name_prefix = $name_prefix;
+
+        return $this;
+    }
+
+    public function getFirstName(): ?string
+    {
+        return $this->first_name;
+    }
+
+    public function setFirstName(string $first_name): self
+    {
+        $this->first_name = $first_name;
+
+        return $this;
+    }
+
+    public function getMidleName(): ?string
+    {
+        return $this->midle_name;
+    }
+
+    public function setMidleName(string $midle_name): self
+    {
+        $this->midle_name = $midle_name;
+
+        return $this;
+    }
+
+    public function getLastName(): ?string
+    {
+        return $this->last_name;
+    }
+
+    public function setLastName(string $last_name): self
+    {
+        $this->last_name = $last_name;
+
+        return $this;
+    }
+
+    public function getGender(): ?string
+    {
+        return $this->gender;
+    }
+
+    public function setGender(string $gender): self
+    {
+        $this->gender = $gender;
+
+        return $this;
+    }
+
+    public function getEmail(): ?string
+    {
+        return $this->email;
+    }
+
+    public function setEmail(string $email): self
+    {
+        $this->email = $email;
+
+        return $this;
+    }
+
+    public function getDateBirth(): ?\DateTimeInterface
+    {
+        return $this->date_birth;
+    }
+
+    public function setDateBirth(\DateTimeInterface $date_birth): self
+    {
+        $this->date_birth = $date_birth;
+
+        return $this;
+    }
+
+    public function getTimeBirth(): ?\DateTimeInterface
+    {
+        return $this->time_birth;
+    }
+
+    public function setTimeBirth(\DateTimeInterface $time_birth): self
+    {
+        $this->time_birth = $time_birth;
+
+        return $this;
+    }
+
+    public function getAgeBirth(): ?\DateInterval
+    {
+        return $this->age_birth;
+    }
+
+    public function setAgeBirth(\DateInterval $age_birth): self
+    {
+        $this->age_birth = $age_birth;
+
+        return $this;
+    }
+
+    public function getDateJoin(): ?\DateTimeInterface
+    {
+        return $this->date_join;
+    }
+
+    public function setDateJoin(?\DateTimeInterface $date_join): self
+    {
+        $this->date_join = $date_join;
+
+        return $this;
+    }
+
+    public function getAgeInCompany(): ?\DateInterval
+    {
+        return $this->age_in_company;
+    }
+
+    public function setAgeInCompany(?\DateInterval $age_in_company): self
+    {
+        $this->age_in_company = $age_in_company;
+
+        return $this;
+    }
+
+    public function getPhone(): ?string
+    {
+        return $this->phone;
+    }
+
+    public function setPhone(?string $phone): self
+    {
+        $this->phone = $phone;
+
+        return $this;
+    }
+
+    public function getPlace(): ?string
+    {
+        return $this->place;
+    }
+
+    public function setPlace(?string $place): self
+    {
+        $this->place = $place;
+
+        return $this;
+    }
+
+    public function getCountry(): ?string
+    {
+        return $this->country;
+    }
+
+    public function setCountry(?string $country): self
+    {
+        $this->country = $country;
+
+        return $this;
+    }
+
+    public function getCity(): ?string
+    {
+        return $this->city;
+    }
+
+    public function setCity(?string $city): self
+    {
+        $this->city = $city;
+
+        return $this;
+    }
+
+    public function getZip(): ?string
+    {
+        return $this->zip;
+    }
+
+    public function setZip(?string $zip): self
+    {
+        $this->zip = $zip;
+
+        return $this;
+    }
+
+    public function getRegion(): ?string
+    {
+        return $this->region;
+    }
+
+    public function setRegion(?string $region): self
+    {
+        $this->region = $region;
+
+        return $this;
+    }
+}
